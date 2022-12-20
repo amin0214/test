4d9ccc5d8fa1120739f960923363ab5acdab37c1
james <james@chsc.tw>
2022-12-20 16:07:40 +0800

    修改一些些

--- oop/main.py
+++ oop/main.py
@@ -5,9 +5,5 @@ region = Region(Point(0,0), Point(100,100))
 snake = Snake(region)
 # snake.head = region.get_rnd_point()
 
-# for i in range(10):
-#     pt = region.get_rnd_point()
-#     print(pt.x, pt.y)
-
 snake.go("右")
 if snake.is_die(): pass
